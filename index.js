97ae4c73c677719f99f5197fb8d19b5dc82e8761
GuccIsabelle <vallasmarius@gmail.com>
2020-08-19 18:33:37 +0200

    updated to use reaction

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", (msg) => {
-  if (/^(heho+)$/i.test(msg.content)) {
-    msg.channel.send(new Discord.MessageAttachment("./heho.jpg"));
-  }
+    if (/^(heho+)$/i.test(msg.content)) {
+        msg.react("745675324481601668");
+        msg.channel.send("<:heho:745675324481601668>");
+    }
 });
 
 client.login(process.env.TOKEN);
